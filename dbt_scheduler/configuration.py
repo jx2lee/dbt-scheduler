ea1163e76cb56a78428e5def8cc1e02f3321a1ff
jx2lee <dev.jaejun.lee.1991@gmail.com>
2024-11-16 11:29:50 +0900

    check scheduler home

--- dbt_scheduler/configuration.py
+++ dbt_scheduler/configuration.py
@@ -13,7 +13,12 @@ def expand_env_var(env_var: str | None) -> str | None:
 
 
 def get_dbt_scheduler_home():
-    return expand_env_var(os.environ.get('DBT_SCHEDULER_HOME', '~/dbt-scheduler'))
+    dbt_scheduler_home = expand_env_var(os.environ.get('DBT_SCHEDULER_HOME', '~/dbt-scheduler'))
+    
+    if not os.path.exists(dbt_scheduler_home):
+        os.makedirs(dbt_scheduler_home, mode=0o755, exist_ok=True)
+            
+    return dbt_scheduler_home
 
 
 DBT_SCHEDULER_HOME = get_dbt_scheduler_home()
